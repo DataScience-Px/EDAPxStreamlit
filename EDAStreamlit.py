571ddb0b48dcacbf8aeab4f7e0ae28692ce30a2b
Génesis <genesis@paradox-ext.com>
2023-06-05 17:41:19 +0200

    update

--- EDAStreamlit.py
+++ EDAStreamlit.py
@@ -14,17 +14,9 @@ dfkaj = pd.read_csv(f'dfkaj_proc.csv', sep=';')
 dfkaj = pd.DataFrame(dfkaj)
 
 
-#Loading the data
-#cols = ['Product Progress','Logins','Start Date', 'Last Activity At', 'Days Lapsed','Months Lapsed']
-#dfkaj = pd.read_csv('https://github.com/DataScience-Px/EDAPxStreamlit/blob/main/dfclean.csv', sep=';', encoding='utf-8', engine='python', 
- #                   on_bad_lines='skip', names=cols, header=0, decimal= '.', parse_dates=['Start Date', 'Last Activity At'], index_col='Unnamed: 0')
-#Creating a dataframe
-#dfkaj = pd.DataFrame(dfkaj)
-
-
 #Erase outliers in the 'Logins' column
-#dfloginskaj = dfkaj[(dfkaj['Logins'] > dfkaj['Logins'].quantile(0.05)) &
-                             #  (dfkaj['Logins'] < dfkaj['Logins'].quantile(0.95))]
+dfloginskaj = dfkaj[(dfkaj['Logins'] > dfkaj['Logins'].quantile(0.05)) &
+                     (dfkaj['Logins'] < dfkaj['Logins'].quantile(0.95))]
 
 
 
