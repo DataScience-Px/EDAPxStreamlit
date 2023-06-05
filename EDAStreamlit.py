4836cd3143a49e2d3c1d0fb56b2adec81f06109c
Génesis <genesis@paradox-ext.com>
2023-06-05 17:26:21 +0200

    update

--- EDAStreamlit.py
+++ EDAStreamlit.py
@@ -9,7 +9,7 @@ import streamlit as st
 import plotly.graph_objects as go
 
 #Loading the data
-dfkaj = pd.read_csv('https://github.com/DataScience-Px/EDAPxStreamlit/blob/main/dfkaj_proc.csv', on_bad_lines='skip')
+dfkaj = pd.read_csv('https://github.com/DataScience-Px/EDAPxStreamlit/blob/main/dfkaj_proc.csv')
 #Creating a dataframe
 dfkaj = pd.DataFrame(dfkaj)
 
